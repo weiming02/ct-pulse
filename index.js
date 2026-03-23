a4a0a8057aee8037b814fc0405bde332bdfbe48d
weiming02 <weimingng02@gmail.com>
2026-03-23 12:08:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,8 +78,12 @@ async function etherscanFetch(chainNumericId, params) {
   if (!ETHERSCAN_API_KEY) return null;
   try {
     const url = new URL('https://api.etherscan.io/v2/api');
-    Object.entries({ ...params, chainid: chainNumericId, apikey: ETHERSCAN_API_KEY }).forEach(([k, v]) => url.searchParams.set(k, v));
-    const res = await fetch(url.toString());
+    Object.entries({ ...params, chainid: chainNumericId }).forEach(([k, v]) => url.searchParams.set(k, v));
+    const res = await fetch(url.toString(), {
+      headers: {
+        'X-Apikey': 8AQYZRBUJZ8M11P4Y4ABNTXR4U8YXTQP4B
+      }
+    });
     const data = await res.json();
     if (data.status === '0' && data.message === 'NOTOK') return null;
     return data.result;
